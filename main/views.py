14001275bb4349fcfcfcdbbe99abe4a99d04434e
Daffa Zuhdii <daffaakmal2101@gmail.com>
2023-09-11 21:11:57 +0800

    add table

--- main/views.py
+++ main/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 
+from .models import Item
 # Create your views here.
 
 
 def show_main(request):
+    item = Item(name='barang1', amount=5, category="kategori1",
+                description="description1")
     context = {
         'name': 'Daffa Akmal Zuhdii',
-        'class': 'PBP A'
+        'class': 'PBP A',
+        'item_name': item.name,
+        'amount': item.amount,
+        'category': item.category,
+        'description': item.description,
+
     }
 
     return render(request, "main.html", context)
